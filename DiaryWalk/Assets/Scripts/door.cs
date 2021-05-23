97d2404bbc734199c6ecd85d086a52359f79669e
dyddndn <72632207+dyddndn@users.noreply.github.com>
2021-05-23 20:40:29 +0900

    DiaryWalk
    DiaryWalk

---
+++ DiaryWalk/Assets/Scripts/door.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour {
+    public Sprite newdoorSprite;
+    public Sprite olddoorSprite;
+
+    public GameObject InTarget; // In Portal
+    public GameObject OutTarget; // out Portal
+    
+    private GameObject PlayerTarget; // Get Player Object
+
+    private Transform InTarget_Trans;
+    private Transform OutTarget_Trans;
+
+    SpriteRenderer olddoor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InTarget_Trans = GetComponent<Transform>();
+        OutTarget_Trans = GetComponent<Transform>();
+
+        InTarget = this.gameObject;
+        InTarget_Trans = InTarget.transform;
+        OutTarget_Trans = OutTarget.transform;
+    }
+    public static door instance; //싱글톤 선언
+    void Awake()
+    {
+    door.instance = this; //싱글톤 부여
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag=="Player") {
+            changedoorsprite();
+		}
+    }
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag=="Player") {
+            changedoorspriteback();
+		  }
+    }
+
+    void changedoorsprite() {
+      olddoor = this.gameObject.GetComponent<SpriteRenderer>();
+        olddoor.sprite = newdoorSprite;
+    }
+    void changedoorspriteback() {
+      olddoor = this.gameObject.GetComponent<SpriteRenderer>();
+        olddoor.sprite = olddoorSprite;
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "Player" && Input.GetKeyDown(KeyCode.E)) // Player Push Portal
+        {
+            PlayerTarget = other.gameObject; // Get Player
+            UsePortal();
+        }
+    }
+    void UsePortal()
+    {
+        PlayerTarget.transform.SetPositionAndRotation(OutTarget_Trans.position,new Quaternion(0.0f,0.0f,0.0f,0.0f));
+    }
+}
---
+++ DiaryWalk/Assets/Titlescene/SceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void StartGame() {
+        gamemanager.nownum=0;
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void LoadGame() {
+        gamemanager.nownum=1;
+        SceneManager.LoadScene("SampleScene");
+        //DontDestroyOnLoad(LoadObject);
+    }
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
