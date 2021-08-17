ee987f9f643ee6cd3c343d5e0f8ec22c8fe22da5
JuYongWoo <72632207+dyddndn@users.noreply.github.com>
2021-08-17 14:06:54 +0900

    bug fix
    bug fix

--- DiaryWalk/Assets/Camera_move.cs
+++ DiaryWalk/Assets/Camera_move.cs
@@ -37,13 +37,11 @@ public class Camera_move : MonoBehaviour {
 
 
 
-	// 캐릭터의 위에 따라 카메라가 이동하도록 하는 메서드
+	// 캐릭터의 위치에 따라 카메라가 이동
 
 	void FixedUpdate () {
 
 		float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-
-		// Mathf.SmoothDamp는 천천히 값을 증가시키는 메서드이다.
 
 		float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
 
@@ -55,7 +53,6 @@ public class Camera_move : MonoBehaviour {
 
 		if(bound) {
 
-			//Mathf.Clamp(현재값, 최대값, 최소값);  현재값이 최대값까지만 반환해주고 최소값보다 작으면 그 최소값까지만 반환합니다.
 
 			transform.position = new Vector3 (Mathf.Clamp (transform.position.x, minPos.x, maxPos.x),
 
